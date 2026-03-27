09bdb667b2b58b61f76dbd2e5494280c1a83adbd
Dylan Soto <dsoto@bmpamerica.com>
2026-03-26 18:28:28 -0600

    fix imports for render

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
-from app.core.config import get_settings
-from app.routers.movies import router as movies_router, tmdb_client
-from app.routers.watch import router as watch_router
+from core.config import get_settings
+from routers.movies import router as movies_router, tmdb_client
+from routers.watch import router as watch_router
 
 settings = get_settings()
 
